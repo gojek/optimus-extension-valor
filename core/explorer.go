7f06fcfe1b15e54c591185641fe9e013bd521442
Anwar Hidayat <15167551+irainia@users.noreply.github.com>
2023-11-23 15:44:43 +0700

    feat: update path explorer to accept regex pattern

--- core/explorer.go
+++ core/explorer.go
@@ -1,18 +1,23 @@
 package core
 
 import (
+	"regexp"
 	"strings"
 
 	"github.com/gojek/optimus-extension-valor/registry/explorer"
 )
 
 // ExplorePaths explores the given root path for the type and format
-func ExplorePaths(rootPath, _type, format string) ([]string, error) {
+func ExplorePaths(rootPath, _type, format, regexPattern string) ([]string, error) {
 	exPath, err := explorer.Explorers.Get(_type)
 	if err != nil {
 		return nil, err
 	}
+	reg, err := regexp.Compile(regexPattern)
+	if err != nil {
+		return nil, err
+	}
 	return exPath(rootPath, func(path string) bool {
-		return strings.HasSuffix(path, format)
+		return reg.MatchString(path) && strings.HasSuffix(path, format)
 	})
 }
